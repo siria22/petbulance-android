776418371590394b531c0169c172a4a8f7ba90f2
siria22 <cmsiria22@gmail.com>
2026-01-21 17:45:32 +0900

    Fix - Lint failure (2)
    - 대문자 -> 소문자

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
-        manifestPlaceholders["KAKAO_NATIVE_APP_KEY"] = localProperties["KAKAO_NATIVE_APP_KEY"] as String? ?: ""
+        manifestPlaceholders["kakao_native_app_key"] = localProperties["KAKAO_NATIVE_APP_KEY"] as String? ?: ""
     }
 
     buildTypes {
