b3f53a2ab4fd6e948135b88bf903f1de2b6813db
Sirius_Alpha <scgv2222@naver.com>
2026-04-18 13:50:36 +0900

    chore - app 버전 (1.0.0)
    - version Code = 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.petbulance.petbulance"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
+        versionCode = 11
         versionName = "v1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
